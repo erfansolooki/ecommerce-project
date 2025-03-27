75740d6139c4ff8852935fc27aa47b287cb57eb8
erfansolooki <erfansolookideveloper@gmail.com>
2025-03-27 22:57:29 +0330

    create order components and route variables

---
+++ src/routes/routesVar.ts
@@ -0,0 +1,5 @@
+export const NOT_FOUND_ROUTE = "*";
+export const BASE_ROUTE = "/";
+export const HOME_ROUTE = BASE_ROUTE + "home";
+export const SHOPPING_CART_ROUTE = BASE_ROUTE + "shopping-cart";
+export const ORDER_ROUTE = BASE_ROUTE + "order";
