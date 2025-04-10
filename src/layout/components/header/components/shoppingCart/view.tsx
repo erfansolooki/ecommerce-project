c4595160a04ecbe4b439724ae7226c235c4c1a51
erfansolooki <erfansolookideveloper@gmail.com>
2025-04-10 22:12:24 +0330

    create search bar

--- src/layout/components/header/components/shoppingCart/view.tsx
+++ src/layout/components/header/components/shoppingCart/view.tsx
@@ -5,7 +5,10 @@ import { RootState } from "../../../../../helpers/features/store";
 export const ShoppingCart = () => {
   const { inShoppingCartCount } = useSelector((state: RootState) => state.app);
   return (
-    <div className="cursor-pointer relative bg-[#232327] w-12 h-12 rounded-full flex justify-center items-center p-1.5">
+    <div
+      className="cursor-pointer relative bg-[#232327] w-12 h-12 rounded-full flex justify-center
+     items-center p-1.5 md:mx-4"
+    >
       {inShoppingCartCount > 0 ? (
         <div className="absolute -top-2 -right-1 bg-[#F2655D] rounded-full w-5 h-5 flex items-center justify-center p-1 text-[12px]">
           {inShoppingCartCount}
--- src/layout/components/header/view.tsx
+++ src/layout/components/header/view.tsx
@@ -1,17 +1,27 @@
 import { Navbar } from "./components/navbar";
+import { Searchbar } from "./components/searchbar";
 import { ShoppingCart } from "./components/shoppingCart";
 import { UserInfo } from "./components/userInfo";
+import { CiSearch } from "react-icons/ci";
 
 export const Header = () => {
   return (
-    <div className="flex fixed py-4 px-2 top-0 right-0 left-0 justify-between">
-      <UserInfo />
+    <>
+      <div className="flex fixed py-4 px-2 lg:px-8 top-0 right-0 left-0 justify-between">
+        <UserInfo />
 
-      <div className="flex items-center">
-        <ShoppingCart />
+        <div className="flex items-center">
+          {window.innerWidth > 400 && (
+            <CiSearch size={24} fill="#fff" className="mx-2.5" />
+          )}
 
-        {window.innerWidth < 400 ? <Navbar /> : <></>}
+          <ShoppingCart />
+
+          {window.innerWidth > 400 ? <Navbar /> : <></>}
+        </div>
       </div>
-    </div>
+
+      {window.innerWidth < 400 && <Searchbar />}
+    </>
   );
 };
