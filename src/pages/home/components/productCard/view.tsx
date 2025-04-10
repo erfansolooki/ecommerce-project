e18438a84778e3063fafa624e6e2bde0eb8116c4
erfansolooki <erfansolookideveloper@gmail.com>
2025-04-11 00:01:59 +0330

    working on product card

--- src/pages/home/components/productCard/view.tsx
+++ src/pages/home/components/productCard/view.tsx
@@ -1,3 +1,30 @@
+import { TbCurrencyDollar } from "react-icons/tb";
+import { CiHeart } from "react-icons/ci";
+
 export const ProductCard = () => {
-  return <div>ProductCard</div>;
+  return (
+    <div className="bg-[#232327] rounded-2xl">
+      <div></div>
+
+      <div>
+        <div>Snopy Headphone</div>
+
+        <div>Snopy SN- BT96 Pretty Back Bluetooth Headphone</div>
+
+        <div>
+          <div>
+            <div>
+              <TbCurrencyDollar />
+            </div>
+
+            <div>6599</div>
+          </div>
+
+          <div>
+            <CiHeart />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
